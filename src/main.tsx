aac584324309dfc0b22f7386ed105c2c1f250aa4
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-02-06 20:22:35 -0500

    add: pocketbase

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
+import "@mantine/core/styles.css"
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MantineProvider } from "@mantine/core";
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router.tsx";
+import PocketBase from "pocketbase";
+
+declare global {
+    interface Window {
+        pb: InstanceType<typeof PocketBase>;
+    }
+}
+
+window.pb = new PocketBase("https://cafe-granito-marron-pb.fly.dev/");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
