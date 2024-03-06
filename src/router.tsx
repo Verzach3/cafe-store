550623a8fbefb95ac3b491aef967b2f03731c6e8
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2024-03-06 14:10:53 -0500

    upload admin changes

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,8 @@ import Home from "./views/Home.tsx";
 import About from "./views/About.tsx";
 import Products from "./views/Products.tsx";
 import Layout from "./components/Layout.tsx";
+import {Auth} from "./views/Auth.tsx";
+import Admin from "./views/Admin.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -31,4 +33,12 @@ export const router = createBrowserRouter([
       }
     ]
   },
+  {
+    path: "/auth",
+    element: <Auth/>
+  },
+  {
+    path: "/admin",
+    element: <Admin/>
+  }
 ])
